46ba1f187f8a56ae875dd6a39288f6fad07d2e9f
satoteharshal97 <satoteharshal97@gmail.com>
2022-03-09 22:02:42 +0530

    Added  - learnt variables

--- function.js
+++ function.js
@@ -1,11 +1,12 @@
-function myFunction(){
-    console.log("JavaScript Basics");
+
+var a = 5;
+
+function varScope() {
+    var a = 5;
+    var a = 6;
+    console.log("inside function it self", a)
+    {
+        // var a = 5
+        console.log("inside blk", a)
+    }
 }
-myFunction();
-
-var abc = function (p1, p2) {
-
-    console.log(p1 + p2)
-}
-
-abc(3, 3)
