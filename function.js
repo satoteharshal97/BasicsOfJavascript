1db2ce28afba780233793fa515de7854e6e54dab
satoteharshal97 <satoteharshal97@gmail.com>
2022-03-09 22:57:44 +0530

    Added - Few examples

--- function.js
+++ function.js
@@ -1,11 +1,47 @@
-function myFunction(){
-    console.log("JavaScript Basics");
-}
-myFunction();
+/*
+z = 10;
+p = z + 15;
+console.log(p);
+var z;
+var p;
+//Hoisting states that we can declare var after intializing variables
 
-var abc = function (p1, p2) {
 
-    console.log(p1 + p2)
-}
+//Arrow Function
 
-abc(3, 3)
+let myFunction = (a, b) => a * b;
+console.log(myFunction(10, 2));
+
+hello = () => "Using Arrow Function";
+console.log(hello());
+
+
+//CallBack Function
+function myFirst() {
+    myDisplayer("Hello");
+  }
+  
+  function mySecond() {
+    myDisplayer("Goodbye");
+  }
+console.log(myFirst());
+console.log(mySecond()); 
+
+*/
+//Asynchronous
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
+  
+  asyncCall()
