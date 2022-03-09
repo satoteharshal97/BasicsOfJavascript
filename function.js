265e1ba6d819f33c1a40df570e228c7047420599
satoteharshal97 <satoteharshal97@gmail.com>
2022-03-09 22:06:58 +0530

    Added - let ,var ,const uses and scope

--- function.js
+++ function.js
@@ -10,3 +10,30 @@ function varScope() {
         console.log("inside blk", a)
     }
 }
+
+
+function letScope() {
+    // let a = 4;
+    // console.log("inside fun", a)
+
+    {
+        const a = 4;
+        //const a = 5;
+        console.log("inside blk", a)
+    }
+
+    // console.log("inside function it self", a)
+
+}
+// console.log("outside function it self", a)
+
+letScope();
+
+// console.log("out side of function", a)
+
+varScope()
+
+var x = () => {
+    console.log("hello")
+}
+x()
