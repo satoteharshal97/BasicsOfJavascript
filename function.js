7b2541c9ee25714a73013e9771a7eb5071eb988a
satoteharshal97 <satoteharshal97@gmail.com>
2022-03-09 22:13:59 +0530

    Added - Parameterized functiion

--- function.js
+++ function.js
@@ -1,11 +1,4 @@
-function myFunction(){
-    console.log("JavaScript Basics");
-}
-myFunction();
-
-var abc = function (p1, p2) {
-
-    console.log(p1 + p2)
-}
-
-abc(3, 3)
+function myFunction(a, b) {
+    return a * b;
+  }
+  myFunction(10, 2);        
